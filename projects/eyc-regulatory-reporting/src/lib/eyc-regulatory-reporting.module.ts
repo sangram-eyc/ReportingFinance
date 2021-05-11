2e99752693f032c043b6ac83a9c65c079a60edb4
raymunoz94 <ray.a.munoz@ey.com>
2021-05-10 22:05:05 -0400

    View filing history and search updates

--- projects/eyc-regulatory-reporting/src/lib/eyc-regulatory-reporting.module.ts
+++ projects/eyc-regulatory-reporting/src/lib/eyc-regulatory-reporting.module.ts
@@ -2,23 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegulatoryReportingFilingComponent } from './regulatory-reporting-filing/components/regulatory-reporting-filing.component';
 import { FilingCardComponent } from './shared/filing-card/filing-card.component';
-import { MotifCardModule, MotifButtonModule, MotifIconModule, MotifProrgressIndicatorsModule} from '@ey-xd/ng-motif';
+import { MotifCardModule, MotifButtonModule, MotifIconModule, MotifProrgressIndicatorsModule, MotifTableModule, MotifFormsModule } from '@ey-xd/ng-motif';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { AgGridModule } from 'ag-grid-angular';
+import { TableHeaderRendererComponent } from './shared/table-header-renderer/table-header-renderer.component';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 
 
 @NgModule({
   imports: [
+    AgGridModule.withComponents([]),
     CommonModule,
     MotifCardModule,
     MotifButtonModule,
+    MotifFormsModule,
     MotifIconModule,
     MotifProrgressIndicatorsModule,
-    SlickCarouselModule
+    MotifTableModule,
+    SlickCarouselModule,
+    InlineSVGModule.forRoot()
   ],
   declarations: [
     RegulatoryReportingFilingComponent,
-    FilingCardComponent
+    FilingCardComponent,
+    TableHeaderRendererComponent
   ],
   exports: [RegulatoryReportingFilingComponent]
 })
