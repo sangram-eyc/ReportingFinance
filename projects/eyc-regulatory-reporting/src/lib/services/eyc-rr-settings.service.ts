f127ee8ffa68e100f27925b41c4b9a4c73e18e02
DD <Ddash@eycomply.local>
2021-05-13 21:53:43 +0530

    feature-environment-configure-changes-lib

--- projects/eyc-regulatory-reporting/src/lib/services/eyc-rr-settings.service.ts
+++ projects/eyc-regulatory-reporting/src/lib/services/eyc-rr-settings.service.ts
@@ -6,5 +6,6 @@ import { Injectable,Inject } from '@angular/core';
 export class EycRrSettingsService {
 
   constructor(@Inject('apiEndpoint') private apiEndpoint) { }
-  public API_ENDPOINT = this.apiEndpoint;
+  public API_ENDPOINT = this.apiEndpoint.slice(-1) === "." ?
+   this.apiEndpoint.substr(0,  this.apiEndpoint.length - 1) :this.apiEndpoint;
 }
