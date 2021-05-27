b60a11f6679a52989a3d2e82dc18f8d3c7a786d5
DD <Ddash@eycomply.local>
2021-05-27 21:26:38 +0530

    feature_integration_testing_user_admin

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -117,9 +117,9 @@ setToken = (value) => {
         //set the authdetails to authconfig to initialize the implict login
         authConfig.loginUrl = res['authenticationUrl'];
         authConfig.logoutUrl = res['logoutUrl'];
-        authConfig.redirectUri = environment.AUTH_PROD ? environment.SERVICE_URL + 'eyc-ServiceEngine-UI/' + res['redirectUrl'] : this.API_ENDPOINT + res['redirectUrl'];
+        authConfig.redirectUri = environment.production ? environment.apiEndpoint +  res['redirectUrl'] : this.API_ENDPOINT + res['redirectUrl'];
         authConfig.clientId = res['clientId'];
-        authConfig.silentRefreshRedirectUri = environment.AUTH_PROD ? environment.SERVICE_URL + 'eyc-ServiceEngine-UI/' + res['silentRefreshRedirectUri'] : this.API_ENDPOINT + res['silentRefreshRedirectUri'];
+        authConfig.silentRefreshRedirectUri = environment.apiEndpoint ? environment.apiEndpoint +  res['silentRefreshRedirectUri'] : this.API_ENDPOINT + res['silentRefreshRedirectUri'];
         authConfig.resource = res['resource'];
         resolve(true);
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   apiEndpoint : '',
-  production: false,
+  production: true,
   SECURITY_ENABLED:true,
-  SERVICE_URL:"https://10.48.234.20/qa31/",
-  /*temp variable needs to be removed after API integration */
-  AUTH_PROD:true
+  // SERVICE_URL:"https://10.48.234.20/qa31/",
+  // /*temp variable needs to be removed after API integration */
+  // AUTH_PROD:true
 };
 
 
