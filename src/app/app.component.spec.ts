e59250c3580d3882bff649d08a54ed44a7f540b5
yarlagadda koteswararao <koteswararao.yarlagadda@ey.com>
2021-06-23 14:22:22 +0530

    Unit test cases for App component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { OAuthLogger, OAuthService, UrlHelperService } from 'angular-oauth2-oidc';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  // let testBedService: UsersService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -15,24 +20,63 @@ describe('AppComponent', () => {
         AppComponent
       ],providers: [OAuthService, UrlHelperService, OAuthLogger ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    
   });
 
-  it(`should have as title 'eyc-ServiceEngine-UI'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('eyc-ServiceEngine-UI');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
+
+  // it( `toggleSubMenu`, () => {
+  //   component.toggleSubMenu('managed-submenu');
+  //   const x = document.getElementById("managed-submenu");
   //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('eyc-ServiceEngine-UI app is running!');
-  // });
+  // }); 
+
+  it( `notification`, () => {
+    component.notification();
+    fixture.detectChanges();
+  });
+
+  it( `navigatetonotifi`, () => {
+    component.navigatetonotifi();
+    fixture.detectChanges();
+  });
+
+  it( `toggleSideNav`, () => {
+    component.toggleSideNav();
+    fixture.detectChanges();
+  });
+
+  it( `toggleSubMenu`, () => {
+    const toggleId = 'managed-submenu';
+    component.toggleSubMenu(toggleId);
+    fixture.detectChanges();
+  });
+
+  it( `logoff`, () => {
+    component.logoff();
+    fixture.detectChanges();
+  });
+
+  it(`outsideClick`, () => {
+    let outSide = component.outsideClick(Event);
+    expect(outSide).toBeFalsy();
+  });
+
+  it(`onMessage`, () => {
+    let msg = component.onMessage(Event);
+    expect(msg).toBeFalsy();
+  });
+
+
 });
