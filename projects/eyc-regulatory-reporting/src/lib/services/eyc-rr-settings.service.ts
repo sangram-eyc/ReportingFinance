414ea0c78ff5c565e671a755fb633d596c99466e
yarlagadda koteswararao <koteswararao.yarlagadda@ey.com>
2021-06-29 10:17:54 +0530

    Truncate long filing names - revert to rest API calls

--- projects/eyc-regulatory-reporting/src/lib/services/eyc-rr-settings.service.ts
+++ projects/eyc-regulatory-reporting/src/lib/services/eyc-rr-settings.service.ts
@@ -14,8 +14,7 @@ export class EycRrSettingsService {
 
   get regReportingFiling(): any {
     const regulatory_Reporting = {
-      filing_details: this.rrproduction ? this.API_ENDPOINT + 'assets/eyc-regulatory-reporting/mock/filings.json' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filings.json',
-      // filing_details: this.rrproduction ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getFilingDetails?filter=active' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filings.json',
+      filing_details: this.rrproduction ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getFilingDetails?filter=active' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filings.json',
       filing_history: this.rrproduction ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getFilingDetails?filter=completed' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filings.json',
       filing_search: this.rrproduction ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/searchFilingDetails?filter=completed&' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filings.json',
       rr_filing_entities: this.rrproduction ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/entity?stage=Reporting' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/filingEntities.json',
