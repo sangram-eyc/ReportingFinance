0458e7a93d092761ed9e3ceb92ebb71b22b8a827
akshayraskar1 <Akshay.Raskar1@ey.com>
2021-07-20 13:13:03 +0530

    Shared grid and modal issue fixed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home.component';
+import { EycUiSharedComponentModule } from 'projects/eyc-ui-shared-component/src/lib/eyc-ui-shared-component.module'
 
 
 @NgModule({
   imports: [
     CommonModule,
+    EycUiSharedComponentModule
   ],
   declarations: [
     HomeComponent
