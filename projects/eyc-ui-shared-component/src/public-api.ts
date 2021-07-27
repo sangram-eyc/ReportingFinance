10ea131208cb6364767c701abcb37f6ba89bba42
raymunoz94 <ray.a.munoz@ey.com>
2021-07-27 00:08:47 -0400

    Grid updates

--- projects/eyc-ui-shared-component/src/public-api.ts
+++ projects/eyc-ui-shared-component/src/public-api.ts
@@ -9,3 +9,6 @@ export * from './lib/eyc-ui-shared-component.module';
 
 export * from './lib/modal/modal.module';
 export * from './lib/modal/component/modal.component';
+
+export * from './lib/grid/grid.module';
+export * from './lib/grid/components/grid.component';
