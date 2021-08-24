7962bb861e81e41c228cf0c45d5843293ceccd20
Gaston Raul Silva <Gaston.Raul.Silva@ey.com>
2021-08-24 20:16:39 -0300

    TA-252522: Fix managementReports endpoint URL

--- projects/eyc-tax-reporting/src/lib/services/eyc-tax-settings.service.ts
+++ projects/eyc-tax-reporting/src/lib/services/eyc-tax-settings.service.ts
@@ -1,6 +1,6 @@
-import { Injectable,Inject } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
-import {SESSION_PBI_TOKEN,PBI_ENCRYPTION_KEY} from '../config/tax-config-helper';
+import { SESSION_PBI_TOKEN, PBI_ENCRYPTION_KEY } from '../config/tax-config-helper';
 
 @Injectable({
   providedIn: 'root'
@@ -14,18 +14,18 @@ export class EycTaxSettingsService {
 
   get taxReportingFiling(): any {
     const tax_Reporting = {
-      filing_details: this.production ? this.API_ENDPOINT + 'taxReportingCoreService/api/v2/taxreporting/managementReports': this.API_ENDPOINT + 'assets/eyc-tax-reporting/mock/filings.json',
-      production_cycles:this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/taxreporting/productionCycles': this.API_ENDPOINT + 'assets/eyc-tax-reporting/mock/ProductionCyclesList.json',
+      filing_details: this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/taxreporting/managementReports' : this.API_ENDPOINT + 'assets/eyc-tax-reporting/mock/filings.json',
+      production_cycles: this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/taxreporting/productionCycles' : this.API_ENDPOINT + 'assets/eyc-tax-reporting/mock/ProductionCyclesList.json',
     }
 
-    
+
 
     return tax_Reporting;
   }
 
-   get pbiReportingConfig(): any {
+  get pbiReportingConfig(): any {
     const regulatory_Reporting = {
-      question_details: this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getQuestionsByFilingId/' : this.API_ENDPOINT +  'assets/eyc-regulatory-reporting/mock/pbi_report.json',
+      question_details: this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getQuestionsByFilingId/' : this.API_ENDPOINT + 'assets/eyc-regulatory-reporting/mock/pbi_report.json',
       pbi_auth_token: this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getPBIAuthToken',
       pbi_embeded_token: this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/getPBIEmbedToken/',
       filing_names: this.production ? this.API_ENDPOINT + 'gatewayService/api/v2/regreporting/filing/names' : this.API_ENDPOINT + 'assets/eyc-regulatory-reporting/mock/filingNames.json',
@@ -34,39 +34,39 @@ export class EycTaxSettingsService {
     }
 
     return regulatory_Reporting;
-  } 
+  }
 
-  setSessionToken = (value,sesssion_id,encrypt_key) => {
+  setSessionToken = (value, sesssion_id, encrypt_key) => {
     const key = CryptoJS.enc.Utf8.parse(sesssion_id);
     const iv = CryptoJS.enc.Utf8.parse(encrypt_key);
     const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
-    {
+      {
         keySize: 128 / 8,
         iv: iv,
         mode: CryptoJS.mode.CBC,
         padding: CryptoJS.pad.Pkcs7
-    });
-    sessionStorage.setItem(SESSION_PBI_TOKEN,encrypted);
-   
+      });
+    sessionStorage.setItem(SESSION_PBI_TOKEN, encrypted);
+
   }
-  
-    getSessionToken = (sesssion_id,encrypt_key) => {
-      const getDecryptedText = sessionStorage.getItem(sesssion_id);
-      const key = CryptoJS.enc.Utf8.parse(encrypt_key);
-      const iv = CryptoJS.enc.Utf8.parse(encrypt_key);
-      if (getDecryptedText != null) {
-        var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
-          keySize: 128 / 8,
-          iv: iv,
-          mode: CryptoJS.mode.CBC,
-          padding: CryptoJS.pad.Pkcs7
-        });
-        return decrypted.toString(CryptoJS.enc.Utf8);
-      }
+
+  getSessionToken = (sesssion_id, encrypt_key) => {
+    const getDecryptedText = sessionStorage.getItem(sesssion_id);
+    const key = CryptoJS.enc.Utf8.parse(encrypt_key);
+    const iv = CryptoJS.enc.Utf8.parse(encrypt_key);
+    if (getDecryptedText != null) {
+      var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return decrypted.toString(CryptoJS.enc.Utf8);
     }
-deleteSessionKey = (session_Key): void => {
-      sessionStorage.removeItem(session_Key);
-};
-  
-    
+  }
+  deleteSessionKey = (session_Key): void => {
+    sessionStorage.removeItem(session_Key);
+  };
+
+
 }
