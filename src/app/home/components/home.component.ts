f9105fbccd3836e71cf55164052654c4db145dc9
akshayraskar1 <Akshay.Raskar1@ey.com>
2021-08-27 14:03:35 +0530

    User Story 258947: Limit access to Hamburger menu options

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {SettingsService} from '../../services/settings.service';
-import {SESSION_ID_TOKEN} from '../../services/settings-helpers';
+import { SettingsService } from '../../services/settings.service';
+import { SESSION_ID_TOKEN } from '../../services/settings-helpers';
 import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from 'projects/eyc-ui-shared-component/src/lib/modal/component/modal.component';
+import { ErrorModalComponent } from 'eyc-ui-shared-component';
+import { ModuleLevelPermissionService } from '@default/services/module-level-permission.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,31 +14,54 @@ import { ModalComponent } from 'projects/eyc-ui-shared-component/src/lib/modal/c
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private settingsService: SettingsService, public dialog: MatDialog) { }
+  moduleLevelPermissionData: any;
+  constructor(
+    private settingsService: SettingsService,
+    public dialog: MatDialog,
+    private moduleLevelPermission: ModuleLevelPermissionService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     if (sessionStorage.getItem(SESSION_ID_TOKEN)) {
       this.settingsService.setIdToken(sessionStorage.getItem(SESSION_ID_TOKEN));
     }
+    this.moduleLevelPermission.getModuleLevelPermission().subscribe(res => {
+      this.moduleLevelPermissionData = res['data'];
+      if (!res['data']['userDetails']) {
+        this.openErrorModal("Access Denied", "User is not a valid user. Please contact admin.");
+      } else if (!res['data']['userDetails'].isActive) {
+        this.openErrorModal("Access Denied", "User is not a active user. Please contact admin.");
+      } else if (!res['data']['modules'].length) {
+        this.openErrorModal("Access Denied", "User does not have access to any module. Please contact admin.");
+      }
+      sessionStorage.setItem("moduleLevelPermission", JSON.stringify(res['data']));
+      this.moduleLevelPermission.invokeModulePermissionDetails();
+    });
   }
 
-  // openDialog() {
-  //   const dialogRef = this.dialog.open(ModalComponent, {
-  //     width: '400px',
-  //     data: {
-  //       type: "Confirmation",
-  //       header: "Approve Selected",
-  //       description: "Are you sure you want to approve the selected entities?",
-  //       footer: {
-  //         style: "start",
-  //         YesButton: "Yes",
-  //         NoButton: "No"
-  //       }
-  //     }
-  //   });
-
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log('The dialog was closed', result);
-  //   });
-  // }
+  openErrorModal(header, description) {
+    const dialogRef = this.dialog.open(ErrorModalComponent, {
+      disableClose: true,
+      width: '400px',
+      data: {
+        header: header,
+        description: description,
+        footer: {
+          style: "start",
+          YesButton: "Okay"
+        },
+      }
+    });
+    if (!this.moduleLevelPermissionData['userDetails']) {
+      setTimeout(() => {
+        dialogRef.close();
+      }, 30000);
+    }
+    dialogRef.afterClosed().subscribe(result => {
+      this.settingsService.logoff();
+      this.router.navigate(['/eyComply'], { queryParams: { logout: true } });
+      console.log('The dialog was closed', result);
+    });
+  }
 }
