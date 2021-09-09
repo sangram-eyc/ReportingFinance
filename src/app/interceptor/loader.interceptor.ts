150415b31cff0d4a96d63a88fdc9cb9c17535e9e
akshayraskar1 <Akshay.Raskar1@ey.com>
2021-09-09 17:45:43 +0530

    Bug 266209: Add comment dialog pop up stays open for some time after clicking submit button

--- src/app/interceptor/loader.interceptor.ts
+++ src/app/interceptor/loader.interceptor.ts
@@ -7,15 +7,25 @@ import { LoaderService } from '../services/loader.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
+    private count = 0;
     constructor(public loaderService: LoaderService) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.loaderService.show();
+        console.log(req);
+        if (this.count === 0) {
+            this.loaderService.show();
+        }
+        this.count++;
         return next.handle(req).pipe(
-            finalize(() => 
-            this.loaderService.hide())
-           /*  setTimeout(() => {
-            this.loaderService.hide() }, 800)
-            ) */
+            finalize(() => {
+                this.count--;
+                if (this.count === 0) {
+                    this.loaderService.hide()
+                }
+            })
+
+            /*  setTimeout(() => {
+             this.loaderService.hide() }, 800)
+             ) */
         );
     }
 }
