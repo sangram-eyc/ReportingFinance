1e93f841ca109dd0c3bd5cd74510412cfbf40385
yarlagadda koteswararao <koteswararao.yarlagadda@ey.com>
2021-09-29 13:39:19 +0530

    View answer exception API changes

--- projects/eyc-regulatory-reporting/src/lib/shared/view-exception-reports/components/view-exception-reports.component.ts
+++ projects/eyc-regulatory-reporting/src/lib/shared/view-exception-reports/components/view-exception-reports.component.ts
@@ -75,7 +75,7 @@ export class ViewExceptionReportsComponent implements OnInit {
   }
 
   getAnswerExceptionReports() {
-    this.viewService.getAnswerExceptionReports(this.filingName, this.period, encodeURIComponent(this.filingService.getExceptionData.exceptionReportName)).subscribe(res => {
+    this.viewService.getAnswerExceptionReports(this.filingName, this.period, this.filingService.getExceptionData.exceptionId).subscribe(res => {
       this.exceptionAnswersData = res.data;
       this.createEntitiesRowData();
     });
