7a57012d3264a4855db2d90adb99f1641dc27d3f
Shivam Srivastava <shivam.srivastava4>
2021-10-14 15:21:49 +0530

    change structure data-Intake component

--- projects/eyc-data-managed-services/src/lib/eyc-data-managed-services.module.ts
+++ projects/eyc-data-managed-services/src/lib/eyc-data-managed-services.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MotifTooltipModule, MotifCardModule, MotifButtonModule, MotifIconModule, MotifProrgressIndicatorsModule, MotifTableModule, MotifFormsModule, MotifTabBarModule, MotifPaginationModule, MotifBreadcrumbModule, MotifChipModule, MotifModalModule, MotifToastModule, MotifDropdownModule } from '@ey-xd/ng-motif';
 import { EycDataManagementServicesComponent } from './eyc-data-managed-services.component';
-import { DataIntakeComponent } from './data-intake/data-intake.component';
+import { DataIntakeComponent } from './data-intake/component/data-intake.component';
 import { EycUiSharedComponentModule } from 'eyc-ui-shared-component';
 
 
