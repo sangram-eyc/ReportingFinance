bd0513d5fdf35febbe8c10906356dd855f45daa6
Prakash Gajera <Prakash.Gajera@ey.com>
2021-10-25 18:18:14 +0530

    Production mode checked

--- projects/eyc-data-managed-services/src/public-api.ts
+++ projects/eyc-data-managed-services/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of eyc-data-managed-services
  */
 
-export * from './lib/eyc-data-managed-services.service';
-export * from './lib/eyc-data-managed-services.component';
+export * from './lib/data-intake/services/data-managed-settings.service';
+export * from './lib//data-intake/component/data-intake.component';
 export * from './lib/eyc-data-managed-services.module';
