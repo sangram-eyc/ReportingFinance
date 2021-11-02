ab7850eed88973c47a06e6a51a91bb83f804b1cb
yarlagadda koteswararao <koteswararao.yarlagadda@ey.com>
2021-11-02 18:35:16 +0530

    Approve filing entity modal popup should contain Yes and No button instead of Continue and Cancel

--- projects/eyc-ui-shared-component/src/lib/grid/components/grid.component.ts
+++ projects/eyc-ui-shared-component/src/lib/grid/components/grid.component.ts
@@ -125,7 +125,7 @@ export class GridComponent implements OnInit, OnChanges {
     console.log('OPEN DIALOG SHARED GRID');
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed', result);
-      if(result.button === "Submit" || result.button === "Continue") {
+      if(result.button === "Submit" || result.button === "Continue" || result.button === "Yes") {
         console.log('Calling Submit ...');
         this.submit();
       } else {
