985796e7fa18be205dc7ab3e7df1710f9bcdd6dc
yarlagadda koteswararao <koteswararao.yarlagadda@ey.com>
2021-12-14 17:20:50 +0530

    enabiling API integartion for DataIntake

--- src/environments/eyc-regulatory-reporting/environment-rr-dev.ts
+++ src/environments/eyc-regulatory-reporting/environment-rr-dev.ts
@@ -4,7 +4,7 @@
 export const environment = {
   apiEndpoint: 'https://10.48.234.20/qa32/',
   production: true,
-  mockDataEnable: true,
+  mockDataEnable: false,
   SECURITY_ENABLED: true,
   SERVICE_URL: "https://10.48.234.20/qa32/",
   AUTH_TYPE: 'OAUTH'
