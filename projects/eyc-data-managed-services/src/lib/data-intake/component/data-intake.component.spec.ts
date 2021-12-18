e6ff98f7f4b136bba26fc9cfe1310887fa686830
Prakash Gajera <Prakash.Gajera@ey.com>
2021-12-18 11:57:35 +0530

    Udpate data intake unit-test

--- projects/eyc-data-managed-services/src/lib/data-intake/component/data-intake.component.spec.ts
+++ projects/eyc-data-managed-services/src/lib/data-intake/component/data-intake.component.spec.ts
@@ -1,6 +1,12 @@
+import { InjectionToken } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { datamanagedenvironment } from '../../../../../../src/environments/eyc-data-managed-services/data-managed-environment';
+import { DataManagedSettingsService } from '../services/data-managed-settings.service';
+import { DataManagedService } from '../services/data-managed.service';
+import { EycDataApiService } from '../services/eyc-data-api.service';
 import { DataIntakeComponent } from './data-intake.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DataIntakeComponent', () => {
   let component: DataIntakeComponent;
@@ -8,7 +14,12 @@ describe('DataIntakeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DataIntakeComponent]
+      declarations: [DataIntakeComponent],
+      providers: [DataManagedService, DataManagedSettingsService,
+        EycDataApiService,
+        { provide: "dataManagedProduction", useValue: datamanagedenvironment.production },
+        { provide: "dataManagedEndPoint", useValue: datamanagedenvironment.apiEndpoint }],
+      imports: [HttpClientTestingModule]
     })
       .compileComponents();
   }));
@@ -16,10 +27,16 @@ describe('DataIntakeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DataIntakeComponent);
     component = fixture.componentInstance;
+    component.ngAfterViewInit();
     fixture.detectChanges();
   });
+
+  // it('should set pageLoaded after view init', () => {
+  //   expect(component.pageLoaded).toBe(true);
+  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- projects/eyc-data-managed-services/src/lib/data-intake/services/data-managed.service.spec.ts
+++ projects/eyc-data-managed-services/src/lib/data-intake/services/data-managed.service.spec.ts
@@ -1,12 +1,24 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { datamanagedenvironment } from '../../../../../../src/environments/eyc-data-managed-services/data-managed-environment';
+import { DataManagedSettingsService } from './data-managed-settings.service';
 
 import { DataManagedService } from './data-managed.service';
+import { EycDataApiService } from './eyc-data-api.service';
 
 describe('DataManagedService', () => {
   let service: DataManagedService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        DataManagedService, 
+        DataManagedSettingsService,
+        EycDataApiService,
+        { provide: "dataManagedProduction", useValue: datamanagedenvironment.production },
+        { provide: "dataManagedEndPoint", useValue: datamanagedenvironment.apiEndpoint }],
+        imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(DataManagedService);
   });
 
