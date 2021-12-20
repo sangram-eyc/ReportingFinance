ce494fceb26a2809492fb5e7f3699631119eebf3
JonnathanCaballero <Jonnathan.Caballero@ey.com>
2021-12-20 17:32:05 -0500

    integrate webSocket

--- projects/eyc-tax-reporting/src/lib/tax-reporting/services/web-socket-bulk.service.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/services/web-socket-bulk.service.ts
@@ -19,9 +19,10 @@ export class WebSocketBulkService {
   
   private getNewWebSocket() {
     const WS_ENDPOINT = this.settingsService.taxReporting.websocket_bulk_url;
+    console.log("url ws ->", WS_ENDPOINT);
     this.socket$ = webSocket({url: WS_ENDPOINT,
       deserializer: ({data}) => data,
-      openObserver: { next: () => {console.log('open conection websocket')}},
+      openObserver: { next: () => {console.log('open conection websocket Bulk')}},
       closeObserver: { next: () => {console.log('closed conection websocket')}}
     });
 
@@ -34,9 +35,9 @@ export class WebSocketBulkService {
       console.log('Object content ->', objectContent);*/
       if(sessionStorage.getItem("pendingDownloadsBulk")){
           //uncomment when the backend is ready
-          const url = "https://eyus.sharepoint.com/sites/SurefootTesting/_layouts/15/download.aspx?UniqueId=52833d2f-bdce-4df4-a4ed-46d2f96888bb&Translate=false&tempauth=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvZXl1cy5zaGFyZXBvaW50LmNvbUA1Yjk3M2Y5OS03N2RmLTRiZWItYjI3ZC1hYTBjNzBiODQ4MmMiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNjQwMDE3NTg1IiwiZXhwIjoiMTY0MDAyMTE4NSIsImVuZHBvaW50dXJsIjoiMjFVcVJrUkZSczF2U0ZvN002MGcvY2poSjVFUC9sNW9jeWRxbjdBUVV3OD0iLCJlbmRwb2ludHVybExlbmd0aCI6IjEzNyIsImlzbG9vcGJhY2siOiJUcnVlIiwiY2lkIjoiTm1Vd05qSTNPREl0Wm1Wa09TMDBOMlJpTFRnek56Z3ROVE14T0RCa05EZGlPVEl4IiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6IlptRTNPVEl6WkdNdE1tRTNOUzAwWW1WbExUa3daall0TURBd1lqRm1aRE5sT1RrNCIsImFwcF9kaXNwbGF5bmFtZSI6IkVZLUdCTC1DVFAtUi1XLU8tQ1RQLUVZQy1RQTM0LVYxLVBSRCIsImdpdmVuX25hbWUiOiJKb25uYXRoYW4iLCJmYW1pbHlfbmFtZSI6IkNhYmFsbGVybyIsInNpZ25pbl9zdGF0ZSI6IltcImttc2lcIixcImR2Y19kbWpkXCJdIiwiYXBwaWQiOiI1YTU2YTU0Ny04NjBlLTQ2YjAtOWIxYi05ZDY4OWY2ZjQ0MWMiLCJ0aWQiOiI1Yjk3M2Y5OS03N2RmLTRiZWItYjI3ZC1hYTBjNzBiODQ4MmMiLCJ1cG4iOiJqb25uYXRoYW4uY2FiYWxsZXJvQGV5LmNvbSIsInB1aWQiOiIxMDAzMjAwMTVGNzhGMDI0IiwiY2FjaGVrZXkiOiIwaC5mfG1lbWJlcnNoaXB8MTAwMzIwMDE1Zjc4ZjAyNEBsaXZlLmNvbSIsInNjcCI6ImFsbHNpdGVzLm1hbmFnZSBhbGxzaXRlcy5yZWFkIGFsbHNpdGVzLndyaXRlIGFsbHByb2ZpbGVzLnJlYWQiLCJ0dCI6IjIiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjpudWxsLCJpcGFkZHIiOiI0MC4xMjYuMjMuMzYifQ.bzhrZ2hYbmdtWDYyODBpRWl5Rzc1dVdsWHRMT3I5aDU1TmtFNGdaZFMzZz0&ApiVersion=2.0";//objectContent.downloadUrl;
+          const url = msg;//objectContent.downloadUrl;
           window.open(url);
-          const id = "c4c48fc2-29d6-452d-a752-618af4e6647c";//objectContent.UIUUID;
+          const id = "12345-6789-9876";//objectContent.UIUUID;
           this.pendingDownloads = JSON.parse(sessionStorage.getItem("pendingDownloadsBulk"));
           this.pendingDownloadsNew = this.pendingDownloads.filter(item => item != id);
           sessionStorage.setItem("pendingDownloadsBulk", JSON.stringify(this.pendingDownloadsNew));
@@ -53,8 +54,9 @@ export class WebSocketBulkService {
   }
 
   sendMessage(uiuuid:string) {
-    let userEmail = sessionStorage.getItem('userEmail');
-    this.socket$.next(userEmail);
+    //let userEmail = sessionStorage.getItem('userEmail');
+    console.log("send to web socket->", uiuuid);
+    this.socket$.next(uiuuid);
     if(sessionStorage.getItem("pendingDownloadsBulk")){
       this.pendingDownloads = JSON.parse(sessionStorage.getItem("pendingDownloadsBulk"));
       this.pendingDownloads.push(uiuuid);
