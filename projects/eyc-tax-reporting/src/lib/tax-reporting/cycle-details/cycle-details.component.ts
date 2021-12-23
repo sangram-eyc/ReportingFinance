ef7aef476a6c7f60a1a4684705390a6ac9ab104f
Diego Morini <Diego.Morini@ey.com>
2021-12-23 14:11:34 -0300

    bugfix-303969-

--- projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
@@ -229,7 +229,6 @@ export class CycleDetailComponent implements OnInit {
     let downloadButton: any = document.querySelector('.second-button');
     downloadButton.insertAdjacentHTML('beforeend', '<svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.25 4.75H8.25V0.25H3.75V4.75H0.75L6 10L11.25 4.75ZM0.75 11.5V13H11.25V11.5H0.75Z" fill="#23232F"/></svg> Download');
     downloadButton.addEventListener('click', this.onClickSecondButton.bind(this));
-    // this.pepe();
   }
 
   showMyAssignedFunds() {
@@ -939,21 +938,6 @@ export class CycleDetailComponent implements OnInit {
       return this.statusColors[1]
     }
   }
-
-
-
-  pepe() {
-    debugger
-    let timerId = setInterval(() => {
-      if (this.processingCheck === 'finished' || this.processingCheck === 'init') {
-        clearInterval(timerId)
-        this.processingCheck = 'init'
-        this.approveBtn.disabled = false;
-      }
-    }, 100);
-  }
-
-
 }
 
 
