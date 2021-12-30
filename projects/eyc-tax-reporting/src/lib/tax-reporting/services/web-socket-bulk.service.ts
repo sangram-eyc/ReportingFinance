08b7f7daec29a90b761d6792b4962b8fa90de9a4
JonnathanCaballero <Jonnathan.Caballero@ey.com>
2021-12-29 22:53:22 -0500

    show error msg in bulk download process

--- projects/eyc-tax-reporting/src/lib/tax-reporting/services/web-socket-bulk.service.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/services/web-socket-bulk.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 import { EycTaxSettingsService } from '../../services/eyc-tax-settings.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorModalComponent } from 'eyc-ui-shared-component';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class WebSocketBulkService {
   private socket$: WebSocketSubject<any>;
   pendingDownloads:string[] = [];     
   pendingDownloadsNew:string[] = [];   
-  constructor(private settingsService: EycTaxSettingsService) { }
+  constructor(private settingsService: EycTaxSettingsService, private dialog: MatDialog,) { }
 
    public connect(): void {
     if (!this.socket$ || this.socket$.closed) {
@@ -20,31 +22,32 @@ export class WebSocketBulkService {
   private getNewWebSocket() {
     let origin = window.location.origin + window.location.pathname;
     let base_url_ws = this.settingsService.taxReporting.websocket_bulk_url;
-    console.log('origin->', origin.trim());
-    console.log('base_url_ws->', base_url_ws.trim());
     let WS_ENDPOINT = this.settingsService.production ? 
                       origin.trim() + base_url_ws.trim(): 
                       this.settingsService.taxReporting.websocket_bulk_url;
     const url_ws = WS_ENDPOINT.replace("https:", "wss:");
-    console.log("url ws v4->", url_ws);
+    console.log("url ws->", url_ws);
     this.socket$ = webSocket({url: url_ws,
-      deserializer: ({data}) => data,
-      openObserver: { next: () => {console.log('open conection websocket Bulk')}},
-      closeObserver: { next: () => {console.log('closed conection websocket')}}
+        deserializer: ({data}) => data,
+        openObserver: { next: () => {console.log('open conection websocket Bulk download')}},
+        closeObserver: { next: () => {console.log('closed conection websocket')}}
     });
 
     this.socket$.subscribe(msg => {
       console.log('message from server: ' + msg);
-      //uncomment when the backend is ready, temporary.
-      /*const objectFromWs = JSON.parse(msg);
-      console.log('Object from ws ->', objectFromWs);
-      const objectContent = JSON.parse(objectFromWs.content);
-      console.log('Object content ->', objectContent);*/
+      const objectFromWs = msg;//JSON.parse(msg);
+      console.log('objectFromWs->', objectFromWs);
       if(sessionStorage.getItem("pendingDownloadsBulk")){
-          //uncomment when the backend is ready
-          const url = msg;//objectContent.downloadUrl;
-          window.open(url);
-          const id = "12345-6789-9876";//objectContent.UIUUID;
+          const url = objectFromWs.value;
+          if(url != ""){
+             window.open(url);
+          }
+          if(objectFromWs.fails > 0){
+             const fundsNames = objectFromWs.failsName.join(',');
+             this.openErrorModal("Error", 
+             "Some of selected files had errors, so that can't be downloaded. Please reload the page and try again the missing file(s) from : " + fundsNames + ".");
+          }
+          const id = objectFromWs.key;
           this.pendingDownloads = JSON.parse(sessionStorage.getItem("pendingDownloadsBulk"));
           this.pendingDownloadsNew = this.pendingDownloads.filter(item => item != id);
           sessionStorage.setItem("pendingDownloadsBulk", JSON.stringify(this.pendingDownloadsNew));
@@ -60,17 +63,16 @@ export class WebSocketBulkService {
     );
   }
 
-  sendMessage(uiuuid:string) {
-    //let userEmail = sessionStorage.getItem('userEmail');
-    console.log("send to web socket->", uiuuid);
-    this.socket$.next(uiuuid);
+  sendMessage(downloadId:string) {
+    console.log("send to web socket->", downloadId);
+    this.socket$.next(downloadId);
     if(sessionStorage.getItem("pendingDownloadsBulk")){
       this.pendingDownloads = JSON.parse(sessionStorage.getItem("pendingDownloadsBulk"));
-      this.pendingDownloads.push(uiuuid);
+      this.pendingDownloads.push(downloadId);
       sessionStorage.setItem("pendingDownloadsBulk", JSON.stringify(this.pendingDownloads));
       console.log('add id to pendingDownloadsBulk', this.pendingDownloads);
      }else{
-      this.pendingDownloads.push(uiuuid); 
+      this.pendingDownloads.push(downloadId);
       sessionStorage.setItem("pendingDownloadsBulk", JSON.stringify(this.pendingDownloads));
       console.log('create and add id to pendingDownloadsBulk', this.pendingDownloads);
      }
@@ -83,5 +85,20 @@ export class WebSocketBulkService {
    unSubscribe(){
     this.socket$.unsubscribe();
    }
+
+   openErrorModal(header, description) {
+    const dialogRef = this.dialog.open(ErrorModalComponent, {
+      disableClose: true,
+      width: '400px',
+      data: {
+        header: header,
+        description: description,
+        footer: {
+          style: "start",
+          YesButton: "OK"
+        },
+      }
+    });
+  }
 
 }
