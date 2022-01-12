03a164261137ea4dd438c1bda85ec63f9da828f4
BU182UK <Szymon.Wozniak@gds.ey.com>
2022-01-12 12:56:19 +0100

    notifications - 299267 - calculaiton date

--- src/app/notifications/notification-item/notification-item.component.ts
+++ src/app/notifications/notification-item/notification-item.component.ts
@@ -29,13 +29,31 @@ export class NotificationItemComponent implements OnInit {
     this.archiveNotification.emit();
   }
 
-  // We Should not a call a method from the template, it will hamper the performance. We need to implement a pipe to calculate the time or this prop should come from the API - By Debabrata 12-01-2022
-  calculateNotificationTime(dateSent) {
-    const sentOnDate = new Date(dateSent);
-    const differenceInDays =
-      (Math.abs(sentOnDate.getTime() - new Date().getTime()) / (1000 * 60)) % 60;
-    return Math.ceil(differenceInDays) + 'min';
+  calculateNotificationTime(date) {
+    // @ts-ignore
+    const seconds = Math.floor((new Date() - new Date(date)) / 1000);
 
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + ' y';
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + ' m';
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + ' d';
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + ' h';
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + ' min';
+    }
+    return Math.floor(seconds) + ' sec';
   }
-  
 }
