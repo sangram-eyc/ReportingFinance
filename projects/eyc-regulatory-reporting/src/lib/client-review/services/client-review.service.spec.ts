49003521097a89a81ee161ad743fedf831555244
Nitin <91242045+nitinmadake1@users.noreply.github.com>
2022-01-19 17:55:07 +0530

    User Story 309487: Unit test cases for client review component and services

--- projects/eyc-regulatory-reporting/src/lib/client-review/services/client-review.service.spec.ts
+++ projects/eyc-regulatory-reporting/src/lib/client-review/services/client-review.service.spec.ts
@@ -1,19 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { environment } from '../../../../../../src/environments/environment';
+import { EycRrApiService } from '../../services/eyc-rr-api.service';
 import { EycRrSettingsService } from '../../services/eyc-rr-settings.service';
 
 import { ClientReviewService } from './client-review.service';
 
 describe('ClientReviewService', () => {
   let service: ClientReviewService;
+  let eycRrSettingsServiceStub = {
+    regReportingFiling: {
+      rr_exception_reports: '/rr_exception_reports/',
+      client_review_filing_entities: '/client_review_filing_entities/',
+      approve_client_review_filing_entities: '/approve_client_review_filing_entities/',
+      filing_unapprove: '/filing_unapprove/',
+      approve_answer_exceptions: '/approve_answer_exceptions/',
+      rr_comments: '/rr_comments/'
+    }
+  }
+  let eycRrApiServiceStub = {
+    invokeGetAPI: () => { },
+    invokePutAPI: () => { },
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, HttpClientTestingModule],
-      providers: [EycRrSettingsService,
-        {provide:"apiEndpoint",  useValue: environment.apiEndpoint}, {provide:"rrproduction",  useValue: environment.production}]
+      providers: [
+        { provide: EycRrSettingsService, useValue: eycRrSettingsServiceStub },
+        { provide: EycRrApiService, useValue: eycRrApiServiceStub }]
     });
     service = TestBed.inject(ClientReviewService);
   });
@@ -22,20 +39,82 @@ describe('ClientReviewService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('invokePutAPI  returned Observable should match the right data', () => {
-    const mockUser ={
-      "entityIds": [1,2,3],
-      "filingName": "FormPF",
-      "period": "Q3 2021",
-      "stage": "Client review"
-    };
-    service.approvefilingEntities(mockUser)
-      .subscribe(res => {
-        expect(res).toBeTruthy();
-      });
+  it('getExceptionReports method should call api and fetch exception reports list', () => {
+    spyOn(service['apiService'], 'invokeGetAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    service.getExceptionReports('FDI', '2011', 'Client review');
+    let url = "/rr_exception_reports/&filingName=FDI&period=2011&stage=Client review";
+    expect(service['apiService'].invokeGetAPI).toHaveBeenCalledWith(url);
+  });
 
-    // const req = httpMock.expectOne(url);
-    // expect(req.request.method).toEqual('PUT');
-    // req.flush(mockUser);
+  it('getfilingEntities method should call api and fetch filing Entities', () => {
+    spyOn(service['apiService'], 'invokeGetAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    service.getfilingEntities('FDI', '2011');
+    let url = "/client_review_filing_entities/&filingName=FDI&period=2011";
+    expect(service['apiService'].invokeGetAPI).toHaveBeenCalledWith(url);
+  });
+
+  it('approvefilingEntities method should call api and approve filing Entity', () => {
+    spyOn(service['apiService'], 'invokePutAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    let mockData = {
+      filingName: 'FDI',
+      entityId: '1011'
+    }
+    service.approvefilingEntities(mockData);
+    let url = "/approve_client_review_filing_entities/";
+    expect(service['apiService'].invokePutAPI).toHaveBeenCalledWith(url, mockData);
+  });
+
+  it('unApprovefilingEntities method should call api and unapprove filing Entity', () => {
+    spyOn(service['apiService'], 'invokePutAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    let mockData = {
+      filingName: 'FDI',
+      entityId: '1011'
+    }
+    service.unApprovefilingEntities(mockData);
+    let url = "/filing_unapprove/";
+    expect(service['apiService'].invokePutAPI).toHaveBeenCalledWith(url, mockData);
+  });
+
+  it('approveAnswerExceptions method should call api and approve exception report', () => {
+    spyOn(service['apiService'], 'invokePutAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    let mockData = {
+      exceptionReportName: 'FDI',
+      exceptionId: '1011'
+    }
+    service.approveAnswerExceptions(mockData);
+    let url = "/approve_answer_exceptions/";
+    expect(service['apiService'].invokePutAPI).toHaveBeenCalledWith(url, mockData);
+  });
+
+  it('unApproveAnswerExceptions method should call api and unapprove exception report', () => {
+    spyOn(service['apiService'], 'invokePutAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    let mockData = {
+      exceptionReportName: 'FDI',
+      exceptionId: '1011'
+    }
+    service.unApproveAnswerExceptions(mockData);
+    let url = "/filing_unapprove/";
+    expect(service['apiService'].invokePutAPI).toHaveBeenCalledWith(url, mockData);
+  });
+
+  it('getComments method should call api and fetch exception reports list', () => {
+    spyOn(service['apiService'], 'invokeGetAPI').and.callFake(() => {
+      return of({ data: [] })
+    });
+    service.getComments('filing', '1011');
+    let url = "/rr_comments/";
+    expect(service['apiService'].invokeGetAPI).toHaveBeenCalledWith(url);
   });
 });
