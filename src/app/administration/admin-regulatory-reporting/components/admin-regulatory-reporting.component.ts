366e39f9350592a1819e491ff72b89aa92a92cf1
KotesYarlagadda <80946578+KotesYarlagadda@users.noreply.github.com>
2022-03-24 13:07:50 +0530

    Bug 340274: Showing total records as NaN for teams tab

--- src/app/administration/admin-regulatory-reporting/components/admin-regulatory-reporting.component.ts
+++ src/app/administration/admin-regulatory-reporting/components/admin-regulatory-reporting.component.ts
@@ -165,6 +165,7 @@ export class AdminRegulatoryReportingComponent implements OnInit, OnDestroy {
       this.getFilingAssignments();
       this.teamsService.getTeamsList(this.moduleName,this.currentPage,this.pageSize,this.sort,this.filter).subscribe(resp => {
         this.teamsData = resp.data;
+        this.totalRecords = 10;
         this.totalRecords=resp.totalRecords;
         if (resetData) {
           this.createTeamsRowData();
