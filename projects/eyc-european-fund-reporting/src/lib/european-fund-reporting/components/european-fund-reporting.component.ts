76587786fbb2d199894d6a62752e8f1791f50143
Diego Morini <Diego.Morini@ey.com>
2022-04-12 16:03:21 -0300

    feature-351064

--- projects/eyc-european-fund-reporting/src/lib/european-fund-reporting/components/european-fund-reporting.component.ts
+++ projects/eyc-european-fund-reporting/src/lib/european-fund-reporting/components/european-fund-reporting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EuropeanFundReportingSettingsService } from '../../services/european-fund-reporting-settings.service';
 
 @Component({
   selector: 'lib-european-fund-reporting',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./european-fund-reporting.component.scss']
 })
 export class EuropeanFundReportingComponent implements OnInit {
-
+  urlWamreg_nonprod: string = 'https://fmsp-stg.ey.com/frc/workflow';
+  urlWamreg_prod: string = 'https://fmsp-stg.ey.com/frc/workflow';
+  current_path: string = window.location.href
+  isProduction: boolean;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  redirectURL() {
+    let res = this.current_path.indexOf('TRP');
+    (res !== -1) ? this.isProduction = true : this.isProduction = false;
+   
+    if (this.isProduction) {
+      console.log('production')
+      window.open(this.urlWamreg_prod);
+    }
+    else {
+      window.open(this.urlWamreg_nonprod);
+    }
   }
 
 }
