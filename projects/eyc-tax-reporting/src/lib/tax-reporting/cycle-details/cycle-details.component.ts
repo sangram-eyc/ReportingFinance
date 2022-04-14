80ba67ab4e8e036c11ad72d024722ffaafa5fe29
JonnathanCaballero <Jonnathan.Caballero@ey.com>
2022-04-14 16:34:39 -0500

    Fix the table logic to approve funds

--- projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
@@ -196,7 +196,7 @@ export class CycleDetailComponent implements OnInit {
   blockApprovalProcess: boolean = false;
   waitApproval: boolean = false;
   processingCheck: any = '';
- 
+  setClickApproveButton = false; 
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -218,9 +218,6 @@ export class CycleDetailComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
-    this.cancelbtn = document.querySelector('.second-button');
-    this.approveBtn = document.querySelector('.approve-button button');
-    this.approveBtn.addEventListener("click", this.approveClickEv.bind(this), true)
     this.getCompletedProductCyclesData(this.productCycleId);
     let downloadButton: any = document.querySelector('.second-button');
     downloadButton.insertAdjacentHTML('beforeend', '<svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.25 4.75H8.25V0.25H3.75V4.75H0.75L6 10L11.25 4.75ZM0.75 11.5V13H11.25V11.5H0.75Z" fill="#23232F"/></svg> Download');
@@ -580,6 +577,12 @@ export class CycleDetailComponent implements OnInit {
   }
 
   datasetsReportRowsSelected(event) {
+    if(this.setClickApproveButton === false){
+      this.setClickApproveButton = true;
+      this.cancelbtn = document.querySelector('.second-button');
+      this.approveBtn = document.querySelector('.approve-button button');
+      this.approveBtn.addEventListener("click", this.approveClickEv.bind(this), true);
+    }
     this.cancelbtn.disabled = true;
     this.approveBtn.disabled = true;
     this.datasetsSelectedRows = event;
