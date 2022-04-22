aedb1b6e74dd16933cc962a283f4b660f0d637e1
Diego Morini <Diego.Morini@ey.com>
2022-04-22 18:31:27 -0300

    feature-355857

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
-import {SESSION_ACCESS_TOKEN,SESSION_ENCRYPTION_KEY,SESSION_ID_TOKEN,ID_ENCRYPTION_KEY,SESSION_ENCRYPTION_IV,ID_ENCRYPTION_IV} from './settings-helpers';
+import { SESSION_ACCESS_TOKEN, SESSION_ENCRYPTION_KEY, SESSION_ID_TOKEN, ID_ENCRYPTION_KEY, SESSION_ENCRYPTION_IV, ID_ENCRYPTION_IV } from './settings-helpers';
 import { environment } from '../../environments/environment';
-import { OAuthService} from 'angular-oauth2-oidc';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { Subject } from 'rxjs';
-import {authConfig} from '../login/helpers'
+import { authConfig } from '../login/helpers'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {authorization} from '../helper/api-config-helper';
+import { authorization } from '../helper/api-config-helper';
 import { v4 as uuid } from 'uuid';
 import { ConcurrentSessionsService } from './concurrent-sessions/concurrent-sessions.service'
 
@@ -17,23 +17,23 @@ import { ConcurrentSessionsService } from './concurrent-sessions/concurrent-sess
 export class SettingsService {
   authdetails;
   moduleLevelPermission;
-  constructor(private oauthService: OAuthService,private http: HttpClient,private concurrentSessionService:ConcurrentSessionsService) { }
+  constructor(private oauthService: OAuthService, private http: HttpClient, private concurrentSessionService: ConcurrentSessionsService) { }
   public API_ENDPOINT = environment.apiEndpoint;
   private pendingHTTPRequests$ = new Subject<void>();
-// AUTHTOKEN FUNCTIONS
-setToken = (value) => {
-  const key = CryptoJS.enc.Utf8.parse(SESSION_ENCRYPTION_KEY);
-  const iv = CryptoJS.enc.Utf8.parse(SESSION_ENCRYPTION_IV);
-  const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
-  {
-      keySize: 128 / 8,
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7
-  });
-  sessionStorage.setItem(SESSION_ACCESS_TOKEN,encrypted);
- 
-}
+  // AUTHTOKEN FUNCTIONS
+  setToken = (value) => {
+    const key = CryptoJS.enc.Utf8.parse(SESSION_ENCRYPTION_KEY);
+    const iv = CryptoJS.enc.Utf8.parse(SESSION_ENCRYPTION_IV);
+    const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
+      {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+      });
+    sessionStorage.setItem(SESSION_ACCESS_TOKEN, encrypted);
+
+  }
 
   getToken = () => {
     const getDecryptedText = sessionStorage.getItem(SESSION_ACCESS_TOKEN);
@@ -50,52 +50,52 @@ setToken = (value) => {
     }
   }
 
-  
+
 
   setIdToken = (value) => {
     const key = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_KEY);
     const iv = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_IV);
-    console.log("Initial vector value",iv)
+    console.log("Initial vector value", iv)
     const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
-    {
+      {
         keySize: 128 / 8,
         iv: iv,
         mode: CryptoJS.mode.CBC,
         padding: CryptoJS.pad.Pkcs7
-    });
-    sessionStorage.setItem(SESSION_ID_TOKEN,encrypted);
-   
-  }
-  
-    getIdToken = () => {
-      const getDecryptedText = sessionStorage.getItem(SESSION_ID_TOKEN);
-      const key = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_KEY);
-      const iv = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_IV);
-      if (getDecryptedText != null) {
-        var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
-          keySize: 128 / 8,
-          iv: iv,
-          mode: CryptoJS.mode.CBC,
-          padding: CryptoJS.pad.Pkcs7
-        });
-        return decrypted.toString(CryptoJS.enc.Utf8);
-      }
-    }
+      });
+    sessionStorage.setItem(SESSION_ID_TOKEN, encrypted);
 
-    // decryptToken = (session_id,encryption_key) => {
-    //   const getDecryptedText = sessionStorage.getItem(session_id);
-    //   const key = CryptoJS.enc.Utf8.parse(encryption_key);
-    //   const iv = CryptoJS.enc.Utf8.parse(encryption_key);
-    //   if (getDecryptedText != null) {
-    //     var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
-    //       keySize: 128 / 8,
-    //       iv: iv,
-    //       mode: CryptoJS.mode.CBC,
-    //       padding: CryptoJS.pad.Pkcs7
-    //     });
-    //     return decrypted.toString(CryptoJS.enc.Utf8);
-    //   }
-    // }
+  }
+
+  getIdToken = () => {
+    const getDecryptedText = sessionStorage.getItem(SESSION_ID_TOKEN);
+    const key = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_KEY);
+    const iv = CryptoJS.enc.Utf8.parse(ID_ENCRYPTION_IV);
+    if (getDecryptedText != null) {
+      var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return decrypted.toString(CryptoJS.enc.Utf8);
+    }
+  }
+
+  // decryptToken = (session_id,encryption_key) => {
+  //   const getDecryptedText = sessionStorage.getItem(session_id);
+  //   const key = CryptoJS.enc.Utf8.parse(encryption_key);
+  //   const iv = CryptoJS.enc.Utf8.parse(encryption_key);
+  //   if (getDecryptedText != null) {
+  //     var decrypted = CryptoJS.AES.decrypt(getDecryptedText, key, {
+  //       keySize: 128 / 8,
+  //       iv: iv,
+  //       mode: CryptoJS.mode.CBC,
+  //       padding: CryptoJS.pad.Pkcs7
+  //     });
+  //     return decrypted.toString(CryptoJS.enc.Utf8);
+  //   }
+  // }
 
   public isUserLoggedin = () => {
     const accessToken = sessionStorage.getItem('access_token')
@@ -107,15 +107,15 @@ setToken = (value) => {
     }
   }
 
-  public logoff() {
-		console.log('inside logout');
+  public async logoff() {
+    console.log('inside logout');
     const body = {
       "id": sessionStorage.getItem('session_id')
-     }
-    this.concurrentSessionService.deleteSessionId(body).subscribe();
-		this.oauthService.logOut();
-		sessionStorage.removeItem("currentUserSession");
-		sessionStorage.removeItem('session');
+    }
+    await this.concurrentSessionService.deleteSessionId(body);
+    this.oauthService.logOut();
+    sessionStorage.removeItem("currentUserSession");
+    sessionStorage.removeItem('session');
     sessionStorage.removeItem('permissionList');
     sessionStorage.removeItem('userEmail');
     sessionStorage.removeItem('inActivityTime');
@@ -127,14 +127,14 @@ setToken = (value) => {
   }
 
   public get name() {
-		const claims = this.oauthService.getIdentityClaims();
-		if (!claims) {
-			return null;
-		}
-		return claims;
-	}
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    }
+    return claims;
+  }
 
-  
+
   // Cancel Pending HTTP calls
   public cancelPendingRequests() {
     this.pendingHTTPRequests$.next();
@@ -148,28 +148,28 @@ setToken = (value) => {
     return new Promise((resolve, reject) => {
       //An Http Get to my API to get the available authdetails
       // if (!sessionStorage.getItem(SESSION_ID_TOKEN)) {
-        const headers = new HttpHeaders({
-          'Content-Type': 'application/json',
-          'X-Correlation-ID': uuid()
-          })
-      this.http.get(`${authorization.auth_Details}`, {headers: headers}).subscribe(res => {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'X-Correlation-ID': uuid()
+      })
+      this.http.get(`${authorization.auth_Details}`, { headers: headers }).subscribe(res => {
         //set the authdetails to authconfig to initialize the implict login
         this.authdetails = res;
         this.authdetails.data.sessionTimeout ? sessionStorage.setItem("inActivityTime", this.authdetails.data.sessionTimeout) : sessionStorage.setItem("inActivityTime", '600000');
         authConfig.loginUrl = this.authdetails.data.authenticationUrl;
         authConfig.logoutUrl = this.authdetails.data.logoutUrl;
-        authConfig.redirectUri = environment.production ?  this.authdetails.data.redirectUrl : this.authdetails.data.redirectUrl;
+        authConfig.redirectUri = environment.production ? this.authdetails.data.redirectUrl : this.authdetails.data.redirectUrl;
         authConfig.clientId = this.authdetails.data.clientId;
         authConfig.silentRefreshRedirectUri = environment.production ? this.authdetails.data.silentRefreshRedirectUri : this.authdetails.data.silentRefreshRedirectUri;
         authConfig.resource = this.authdetails.data.resource;
         authConfig.timeoutFactor = environment.production ? this.authdetails.data.timeoutFactor : 0.25;
-        authConfig.silentRefreshTimeout=  environment.production ? this.authdetails.data.timeoutFactor : 5000;
+        authConfig.silentRefreshTimeout = environment.production ? this.authdetails.data.timeoutFactor : 5000;
         resolve(true);
-        
+
       })
-    /* } else {
-      resolve(true);
-    } */
+      /* } else {
+        resolve(true);
+      } */
     });
 
   }
