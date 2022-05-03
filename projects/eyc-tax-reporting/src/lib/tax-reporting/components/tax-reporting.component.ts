373ea61461659e166a58537ee1a035098505e321
JonnathanCaballero <Jonnathan.Caballero@ey.com>
2022-05-03 14:07:18 -0500

    Fix pagination size

--- projects/eyc-tax-reporting/src/lib/tax-reporting/components/tax-reporting.component.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/components/tax-reporting.component.ts
@@ -84,8 +84,8 @@ export class TaxReportingComponent implements OnInit {
   }];
   currentlySelectedPageSize = {
     disable: false,
-    value: 10,
-    name: '10',
+    value: 20,
+    name: '20',
     id: 0
   };
 
--- projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
+++ projects/eyc-tax-reporting/src/lib/tax-reporting/cycle-details/cycle-details.component.ts
@@ -156,8 +156,8 @@ export class CycleDetailComponent implements OnInit {
   }];
   currentlySelectedPageSize = {
     disable: false,
-    value: 5,
-    name: '5',
+    value: 10,
+    name: '10',
     id: 0
   };
 
