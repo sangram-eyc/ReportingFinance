8a729c451bc4d8aea84bf94a6406faab0eb706d1
KotesYarlagadda <80946578+KotesYarlagadda@users.noreply.github.com>
2022-05-30 15:40:23 +0530

    Bug 375451: By default in static data page only 10 records are displaying

--- projects/eyc-ui-shared-component/src/lib/grid/components/grid.component.ts
+++ projects/eyc-ui-shared-component/src/lib/grid/components/grid.component.ts
@@ -161,26 +161,26 @@ pageSize;
     if(this.pagination && !this.paginationApi){
       this.dataset = [{
         disable: false,
-        value: this.paginationSize,
+        value: this.paginationSize * 2,
         name: (this.paginationSize).toString(),
         id: 0
       },
       {
         disable: false,
-        value: this.paginationSize*2,
-        name: (this.paginationSize*2).toString(),
+        value: this.paginationSize*5,
+        name: (this.paginationSize*5).toString(),
         id: 1
       },
       {
         disable: false,
-        value: this.paginationSize*3,
-        name: (this.paginationSize*3).toString(),
+        value: this.paginationSize*10,
+        name: (this.paginationSize*10).toString(),
         id: 2
       }];
 
       this.currentlySelectedPageSize = {
         disable: false,
-        value: this.paginationSize,
+        value:  this.paginationSize * 2,
         name: (this.paginationSize).toString(),
         id: 0
       };
