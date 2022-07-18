74a5f2b7410eefa71fcb017a3179e778c8b5f927
KotesYarlagadda <80946578+KotesYarlagadda@users.noreply.github.com>
2022-07-18 14:20:22 +0530

    Disable additional tabs in team details page

--- projects/eyc-admin/src/lib/config/setting-helper.ts
+++ projects/eyc-admin/src/lib/config/setting-helper.ts
@@ -1,3 +1,4 @@
 export const INPUT_VALIDATION = /[A-Za-z0-9\ ]+/;
 export const IS_USER_DETAILS_EDITABLE = true;
 export const IS_TEAM_DETAILS_EDITABLE = true;
+export const IS_TEAM_ASSIGNMNETS_TABS = false;
