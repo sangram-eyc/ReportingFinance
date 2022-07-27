472433ba2d4d4a1f2e4022e8b6cbe157dbbbe1be
Poorna chandran <poorna.chandran.ramesh@gds.ey.com>
2022-07-27 17:38:01 +0530

    Bug 000000: Check ag-license key

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { LicenseManager } from "ag-grid-enterprise";
+LicenseManager.setLicenseKey("CompanyName=SHI International Corp._on_behalf_of_Ernst & Young U.S. LLP,LicensedApplication=EY Comply,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=11,LicensedProductionInstancesCount=1,AssetReference=AG-027377,ExpiryDate=23_May_2023_[v2]_MTY4NDc5NjQwMDAwMA==9014b013fefbb0b8e447956ac4272e81"
+);
 
 if (environment.production) {
   enableProdMode();
