f7829d0533cfe9001a651d7ddfb34d5b0c046c39
Poorna chandran <poorna.chandran.ramesh@gds.ey.com>
2022-07-27 18:47:53 +0530

    Bug 000000: Check ag-license key update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
   SECURITY_ENABLED: true,
   SERVICE_URL: "https://10.48.234.20/qa32/",
   /*temp variable needs to be removed after API integration */
-  //AUTH_PROD:true
+  //AUTH_PROD:true,
+  AG_License_KEY : "CompanyName=SHI International Corp._on_behalf_of_Ernst & Young U.S. LLP,LicensedApplication=EY Comply,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=11,LicensedProductionInstancesCount=1,AssetReference=AG-027377,ExpiryDate=23_May_2023_[v2]_MTY4NDc5NjQwMDAwMA==9014b013fefbb0b8e447956ac4272e81"
 };
 /*
  * For easier debugging in development mode, you can import the following file
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { LicenseManager } from "ag-grid-enterprise";
-LicenseManager.setLicenseKey("CompanyName=SHI International Corp._on_behalf_of_Ernst & Young U.S. LLP,LicensedApplication=EY Comply,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=11,LicensedProductionInstancesCount=1,AssetReference=AG-027377,ExpiryDate=23_May_2023_[v2]_MTY4NDc5NjQwMDAwMA==9014b013fefbb0b8e447956ac4272e81"
-);
+LicenseManager.setLicenseKey(environment.AG_License_KEY);
 
 if (environment.production) {
   enableProdMode();
